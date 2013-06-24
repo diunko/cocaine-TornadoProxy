28031ed180e8bcf157045288282e4e0dcf929df0
noxiouz <noxiouz@yandex.ru>
2013-06-24 17:28:43 +0400

    Version bump and change package structure

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     url="https://github.com/cocaine/cocaine-Tornado-proxy",
     author="Anton Tyurin",
     author_email="noxiouz@yandex.ru",
+    packages=["cocaineproxy"],
     license="LGPLv3+",
     scripts=['cocaine-tornado-proxy', 'cocaine-tornado-proxy2'],
     data_files=[('/etc/init.d/',['init/cocaine-tornado-proxy']),
